fd1bd05270b9f2ae66b3077f998e8b0e5a080a17
SYegorV <102388341+SYegorV@users.noreply.github.com>
2022-05-22 19:08:45 +0700

    unit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 const output = document.querySelector('output')
 let memoryMS = 0 // определяем memory
 let signMinus = false // true - number отрицательное
+let i = 0
 
 // find button and add in button обработчикa события "click":
 document.querySelectorAll('button').forEach(button => {
@@ -38,7 +39,16 @@ function calc(value) {
         // пробуем выполнить операцию:
         try {
             // вычисляем значение строки
-            output.textContent =  eval(output.textContent);
+            const n = 8 // 8 знаков дробной части к вычисляемому результату
+            let s = eval(output.textContent).toFixed(n) // исходное число // строчка            
+            let f = Math.floor( Number(s)) 
+            let t = Number(s)- f // дробная часть //число            
+            let v = Math.floor( Number(s)) // целая чать
+            let Strt = String(t) // число в строку
+            let Strtt = Strt.replace(/0*$/,'')            
+            let tt = Number(Strtt) // строчка в число
+            output.textContent = Number(`${v + tt}`) // строчка в число
+            
             if (value.match(/=/)) {
                 
             }              
@@ -72,7 +82,7 @@ function calc(value) {
         }                
     } else if (value === 'MR') { // если нажат simbol MR
         // вывести number from memory on индикатор
-        output.textContent = memoryMS.toFixed(4)        
+        output.textContent = memoryMS.toFixed(8)        
     } else if (value === 'M+') { // если нажат simbol M+
         // add to number in memory numbers on индикаторе
         memoryMS = Number(memoryMS) + Number(output.textContent)
@@ -95,13 +105,15 @@ function calc(value) {
     } else if (value.match(/<-/)) { // если нажат simbol <-
         // <- уменьшаем строку на один simbol:
         output.textContent = output.textContent.substring(0, output.textContent.length - 1)
-    } else if (value.match(/-<>/)) { // если нажат символ &plusmn        
+    } else if (value === '-<>') { // если нажат символ &plusmn --- value.match(/-<>/)        
         signMinus = !signMinus //смена знака        
         output.textContent = Number(signMinus ? - + Number(output.textContent): Number(output.textContent))        
     } else if (value.match(/CE|Backspace/)) { // если нажат символ CE или Backspace 
         // CE уменьшаем строку на один simbol
         // Backspace вводим предыдущий [символ/действие] повтор
         output.textContent = output.textContent.substring(0, output.textContent.length - 1)
+    } else if ( value === '.'|value === '*'|value === '/'|value === '+'|value === '-' && output.textContent.substring(output.textContent.length - 1, output.textContent.length) === '.'|output.textContent.substring(output.textContent.length - 1, output.textContent.length) === '*'|output.textContent.substring(output.textContent.length - 1, output.textContent.length) === '/'|output.textContent.substring(output.textContent.length - 1, output.textContent.length) === '+'|output.textContent.substring(output.textContent.length - 1, output.textContent.length) === '-' ) {
+        
     } else if ( output.textContent.length > 32 - 1 ) { // ввод 32 simbols in поле
         let oldValue = output.textContent // save value поля
         let newValue = 'недопустимое выражение' // create new переменную
@@ -110,8 +122,78 @@ function calc(value) {
         setTimeout(() => {
             output.textContent = oldValue
         }, 1500)
-    } else { // если нажата любая другая (отфильтрованная) кнопка или клавиша
+    } else if (value === '.'|value === '0'|value === '1'|value === '2'|value === '3'|value === '4'|value === '5'|value === '6'|value === '7'|value === '8'|value === '9') {
+        const n1 = 12 // 12 знаков целой части
+        const n2 = 8 // 8 знаков дробная часть
+
+        let ss = output.textContent // исходное число        
+        ss = Number(ss + value).toFixed(n2+2)        
+        let ff = Math.floor( Number(ss)).toFixed(n2+2)        
+        let tt = (Number(ss)- ff).toFixed(n2 + 2) // дробная часть // строчка        
+        if (tt > 0 ) {            
+            i = i + 1            
+            zxcone(i, ss, n1, n2, value)            
+        } else {
+            zxctwo(tt, ss, n1, n2, value)
+        }        
+    }
+    else { // если нажата любая другая (отфильтрованная) кнопка или клавиша
         // write value in поле:
         output.textContent += value        
     }
 }
+
+function zxcone(i, ss, n1, n2, value) {
+    let tt = (Number(ss)%1).toFixed(2 + i) // дробная часть // строчка    
+    let replacett = Number((Number(tt.replace(/0*$/,''))).toFixed(i)) // дробная часть // число    
+    let vv = Math.floor( Number(ss)) // целая чать // число
+    let Strvv = String(vv) // число в строку
+    let Strreplacett = String(replacett) // число в строку    
+    if (Strvv.length > n1) {
+        let oldValue = output.textContent // сохраняем значение поля
+        let newValue = 'целая чать более 12 цифр' // создаем новую переменную
+        output.textContent = newValue // выводим значение новой переменной в поле
+        // через полторы секунды возвращаем полю предыдущее значение:
+        setTimeout(() => {
+            output.textContent = oldValue
+        }, 1500)
+    } else if (Strreplacett.length - 2 >= n2 + 1) {
+        let oldValue = output.textContent // сохраняем значение поля
+        let newValue = 'дробная часть более 8 цифр' // создаем новую переменную
+        output.textContent = newValue // выводим значение новой переменной в поле
+        // через полторы секунды возвращаем полю предыдущее значение:
+        setTimeout(() => {
+            output.textContent = oldValue
+        }, 1500)
+    } else { //(Strvv.length < n1||Strreplacett.length - 2 < n2 + 1) {
+        output.textContent += value
+    }
+}
+
+function zxctwo(tt, ss, n1, n2, value) {    
+    let replacett = Number(tt.replace(/0*$/,'')) // дробная часть // число    
+    let vv = Math.floor( Number(ss)) // целая чать // число
+    let Strvv = String(vv) // число в строку
+    let Strreplacett = String(replacett) // число в строку    
+    if (Strvv.length > n1) {
+        let oldValue = output.textContent // сохраняем значение поля
+        let newValue = 'целая чать более 12 цифр' // создаем новую переменную
+        output.textContent = newValue // выводим значение новой переменной в поле
+        // через полторы секунды возвращаем полю предыдущее значение:
+        setTimeout(() => {
+            output.textContent = oldValue
+        }, 1500)
+    } else if (Strreplacett.length - 2 >= n2 + 1) {
+        let oldValue = output.textContent // сохраняем значение поля
+        let newValue = 'дробная часть более 8 цифр' // создаем новую переменную
+        output.textContent = newValue // выводим значение новой переменной в поле
+        // через полторы секунды возвращаем полю предыдущее значение:
+        setTimeout(() => {
+            output.textContent = oldValue
+        }, 1500)
+    } else { //(Strvv.length < n1||Strreplacett.length - 2 < n2 + 1) {
+        output.textContent += value
+    }    
+}
+
+
